b617884fb0347ab88e31e4e25dd45bdf6bab0f51
Gabriel Santos <gabriel.fra.santos@gmail.com>
2024-12-08 14:17:17 +0100

    add interface for simd instructions

---
+++ math/SingleInstructionMultipleData.hpp
@@ -0,0 +1,107 @@
+// doc: https://arm-software.github.io/CMSIS_5/Core/html/group__intrinsic__SIMD__gr.html#gabb5bcba694bf17b141c32e6a8474f60e
+
+uint32_t __PKHBT 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint32_t  	val3 
+	)
+
+uint32_t __PKHTB 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint32_t  	val3 
+	)
+
+uint32_t __QADD 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	)
+    
+uint32_t __QADD16 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	)
+
+uint32_t __QADD8 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __QASX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __QSAX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __QSUB 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __QSUB16 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __QSUB8 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SHADD16 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SHASX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SHSAX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SHSUB16 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SMLAD 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint32_t  	val3 
+	) 	
+
+uint32_t __SMLADX 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint32_t  	val3 
+	)
+
+uint64_t __SMLALD 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint64_t  	val3 
+	)
+
+unsigned long long __SMLALDX 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		unsigned long long  	val3 
+	)
+
+uint32_t __SMLSDX 	( 	uint32_t  	val1,
+		uint32_t  	val2,
+		uint32_t  	val3 
+	) 	
+
+uint32_t __SMMLA 	( 	int32_t  	val1,
+		int32_t  	val2,
+		int32_t  	val3 
+	) 	
+
+uint32_t __SMUAD 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SMUADX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SMUSD 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SMUSDX 	( 	uint32_t  	val1,
+		uint32_t  	val2 
+	) 	
+
+uint32_t __SXTB16 	( 	uint32_t  	val	) 	
