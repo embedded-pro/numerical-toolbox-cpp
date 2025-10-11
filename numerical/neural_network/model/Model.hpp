0a01bff6d611416d97716a0beed16a34e36faefb
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-10-11 17:35:27 +0000

    fix windows build

--- numerical/neural_network/model/Model.hpp
+++ numerical/neural_network/model/Model.hpp
@@ -113,7 +113,7 @@ namespace neural_network
         InputVector Backward(const OutputVector& output_gradient);
         void Train(Optimizer<QNumberType, TotalParameters>& optimizer, Loss<QNumberType, TotalParameters>& loss, const math::Vector<QNumberType, TotalParameters>& initialParameters);
         void SetParameters(const math::Vector<QNumberType, TotalParameters>& parameters);
-        math::Vector<QNumberType, TotalParameters> GetParameters() const;
+        math::Vector<QNumberType, Model::TotalParameters> GetParameters() const;
 
     private:
         template<std::size_t... Is>
@@ -129,7 +129,7 @@ namespace neural_network
         void SetLayerParameters(Layer& layer, const math::Vector<QNumberType, TotalParameters>& parameters, std::size_t& offset);
 
         template<std::size_t... Is>
-        math::Vector<QNumberType, TotalParameters> GetParametersImpl(std::index_sequence<Is...>) const;
+        math::Vector<QNumberType, Model::TotalParameters> GetParametersImpl(std::index_sequence<Is...>) const;
 
         template<typename Layer>
         void GetLayerParameters(const Layer& layer, math::Vector<QNumberType, TotalParameters>& parameters, std::size_t& offset) const;
@@ -170,7 +170,7 @@ namespace neural_network
     }
 
     template<typename QNumberType, std::size_t InputSize, std::size_t OutputSize, typename... Layers>
-    typename math::Vector<QNumberType, Model<QNumberType, InputSize, OutputSize, Layers...>::TotalParameters> Model<QNumberType, InputSize, OutputSize, Layers...>::GetParameters() const
+    math::Vector<QNumberType, Model<QNumberType, InputSize, OutputSize, Layers...>::TotalParameters> Model<QNumberType, InputSize, OutputSize, Layers...>::GetParameters() const
     {
         return GetParametersImpl(std::make_index_sequence<sizeof...(Layers)>{});
     }
