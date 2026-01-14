d144841ae8bc550753b56cddca8228719282fae8
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-01-14 14:27:44 +0000

    fix coverage

---
+++ numerical/controllers/implementations/PidIncremental.cpp
@@ -0,0 +1,13 @@
+#include "numerical/controllers/implementations/PidIncremental.hpp"
+#include "numerical/math/QNumber.hpp"
+
+namespace controllers
+{
+    template class PidIncrementalBase<float>;
+    template class PidIncrementalBase<math::Q15>;
+    template class PidIncrementalBase<math::Q31>;
+
+    template class PidIncrementalSynchronous<float>;
+    template class PidIncrementalSynchronous<math::Q15>;
+    template class PidIncrementalSynchronous<math::Q31>;
+}
