1fbea7682318d46332474a252306d9d0ec3be4bd
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-07-26 20:10:35 +0000

    split declaration from implementation

--- numerical/analysis/GoertzelAlgorithm.hpp
+++ numerical/analysis/GoertzelAlgorithm.hpp
@@ -19,58 +19,16 @@ namespace analysis
         static_assert(std::is_floating_point_v<T>, "GoertzelAlgorithm supports floating-point types only");
 
     public:
-        GoertzelAlgorithm(std::size_t k, std::size_t blockLength)
-            : coeff{ T{ 2 } * std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
-            , cosine{ std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
-            , sine{ std::sin(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
-            , blockSize{ blockLength }
-            , s1{ T{ 0 } }
-            , s2{ T{ 0 } }
-            , sampleCount{ 0 }
-        {}
+        GoertzelAlgorithm(std::size_t k, std::size_t blockLength);
+        GoertzelAlgorithm(T targetHz, T sampleHz, std::size_t blockLength);
 
-        GoertzelAlgorithm(T targetHz, T sampleHz, std::size_t blockLength)
-            : GoertzelAlgorithm(
-                  static_cast<std::size_t>(targetHz / sampleHz * static_cast<T>(blockLength) + T{ 0.5 }),
-                  blockLength)
-        {}
+        OPTIMIZE_FOR_SPEED void Push(T x);
+        bool Ready() const;
+        math::Complex<T> Result() const;
+        T Magnitude() const;
+        void Reset();
 
-        OPTIMIZE_FOR_SPEED void Push(T x)
-        {
-            T s0{ x + coeff * s1 - s2 };
-            s2 = s1;
-            s1 = s0;
-            ++sampleCount;
-        }
-
-        bool Ready() const
-        {
-            return sampleCount >= blockSize;
-        }
-
-        math::Complex<T> Result() const
-        {
-            T real{ s1 - s2 * cosine };
-            T imag{ s2 * sine };
-            return math::Complex<T>{ real, imag };
-        }
-
-        T Magnitude() const
-        {
-            return std::sqrt(s1 * s1 + s2 * s2 - coeff * s1 * s2);
-        }
-
-        void Reset()
-        {
-            s1 = T{ 0 };
-            s2 = T{ 0 };
-            sampleCount = 0;
-        }
-
-        static T Coefficient(std::size_t k, std::size_t blockLength)
-        {
-            return T{ 2 } * std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength));
-        }
+        static T Coefficient(std::size_t k, std::size_t blockLength);
 
     private:
         T coeff;
@@ -81,6 +39,67 @@ namespace analysis
         T s2;
         std::size_t sampleCount;
     };
+
+    template<typename T>
+    GoertzelAlgorithm<T>::GoertzelAlgorithm(std::size_t k, std::size_t blockLength)
+        : coeff{ T{ 2 } * std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
+        , cosine{ std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
+        , sine{ std::sin(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength)) }
+        , blockSize{ blockLength }
+        , s1{ T{ 0 } }
+        , s2{ T{ 0 } }
+        , sampleCount{ 0 }
+    {}
+
+    template<typename T>
+    GoertzelAlgorithm<T>::GoertzelAlgorithm(T targetHz, T sampleHz, std::size_t blockLength)
+        : GoertzelAlgorithm(
+              static_cast<std::size_t>(targetHz / sampleHz * static_cast<T>(blockLength) + T{ 0.5 }),
+              blockLength)
+    {}
+
+    template<typename T>
+    OPTIMIZE_FOR_SPEED void GoertzelAlgorithm<T>::Push(T x)
+    {
+        T s0{ x + coeff * s1 - s2 };
+        s2 = s1;
+        s1 = s0;
+        ++sampleCount;
+    }
+
+    template<typename T>
+    bool GoertzelAlgorithm<T>::Ready() const
+    {
+        return sampleCount >= blockSize;
+    }
+
+    template<typename T>
+    math::Complex<T> GoertzelAlgorithm<T>::Result() const
+    {
+        T real{ s1 - s2 * cosine };
+        T imag{ s2 * sine };
+        return math::Complex<T>{ real, imag };
+    }
+
+    template<typename T>
+    T GoertzelAlgorithm<T>::Magnitude() const
+    {
+        return std::sqrt(s1 * s1 + s2 * s2 - coeff * s1 * s2);
+    }
+
+    template<typename T>
+    void GoertzelAlgorithm<T>::Reset()
+    {
+        s1 = T{ 0 };
+        s2 = T{ 0 };
+        sampleCount = 0;
+    }
+
+    template<typename T>
+    T GoertzelAlgorithm<T>::Coefficient(std::size_t k, std::size_t blockLength)
+    {
+        return T{ 2 } * std::cos(T{ 2 } * std::numbers::pi_v<T> * static_cast<T>(k) / static_cast<T>(blockLength));
+    }
 }
 
 #ifdef NUMERICAL_TOOLBOX_COVERAGE_BUILD
