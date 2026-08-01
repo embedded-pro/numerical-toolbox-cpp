afe7958213c497dca9eaf91e9ac2c969bb5eb0cc
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-08-01 07:18:14 +0000

    remove dynamics from this repo

--- numerical/dynamics/EulerLagrangeSolver.cpp
+++
@@ -1,8 +0,0 @@
-#include "numerical/dynamics/EulerLagrangeSolver.hpp"
-
-namespace dynamics
-{
-    template class EulerLagrangeSolver<float, 1>;
-    template class EulerLagrangeSolver<float, 2>;
-    template class EulerLagrangeSolver<float, 3>;
-}
--- numerical/kinematics/InverseKinematics.cpp
+++
@@ -1,8 +0,0 @@
-#include "numerical/kinematics/InverseKinematics.hpp"
-
-namespace kinematics
-{
-    template class InverseKinematics<float, 1>;
-    template class InverseKinematics<float, 2>;
-    template class InverseKinematics<float, 3>;
-}
