038ccacd3ac0346fbafe98eb39d197511d3ca446
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-08-02 11:05:09 +0000

    improve unit tests for convolution

--- numerical/analysis/test/TestConvolutionCorrelation.cpp
+++ numerical/analysis/test/TestConvolutionCorrelation.cpp
@@ -82,6 +82,8 @@ TEST_F(TestConvolutionCorrelation, convolution_with_unit_impulse_is_identity)
     EXPECT_NEAR(y[0], x[0], math::Tolerance<float>());
     EXPECT_NEAR(y[1], x[1], math::Tolerance<float>());
     EXPECT_NEAR(y[2], x[2], math::Tolerance<float>());
+    EXPECT_NEAR(y[3], 0.0f, math::Tolerance<float>());
+    EXPECT_NEAR(y[4], 0.0f, math::Tolerance<float>());
 }
 
 TEST_F(TestConvolutionCorrelation, convolution_is_commutative)
@@ -103,6 +105,43 @@ TEST_F(TestConvolutionCorrelation, convolution_is_commutative)
 
     for (std::size_t i = 0; i < 5; ++i)
         EXPECT_NEAR(y1[i], y2[i], math::Tolerance<float>());
+}
+
+TEST_F(TestConvolutionCorrelation, linear_convolution_is_linear)
+{
+    Vec3 x1;
+    x1.push_back(1.0f);
+    x1.push_back(0.0f);
+    x1.push_back(-1.0f);
+
+    Vec3 x2;
+    x2.push_back(0.0f);
+    x2.push_back(1.0f);
+    x2.push_back(2.0f);
+
+    Vec3 h;
+    h.push_back(1.0f);
+    h.push_back(2.0f);
+    h.push_back(1.0f);
+
+    constexpr float a{ 3.0f };
+    constexpr float b{ -2.0f };
+
+    Vec3 xCombined;
+    xCombined.push_back(a * x1[0] + b * x2[0]);
+    xCombined.push_back(a * x1[1] + b * x2[1]);
+    xCombined.push_back(a * x1[2] + b * x2[2]);
+
+    Vec5 yCombined;
+    analysis::LinearConvolution<float, 3, 3>(xCombined, h, yCombined);
+
+    Vec5 y1;
+    Vec5 y2;
+    analysis::LinearConvolution<float, 3, 3>(x1, h, y1);
+    analysis::LinearConvolution<float, 3, 3>(x2, h, y2);
+
+    for (std::size_t i = 0; i < 5; ++i)
+        EXPECT_NEAR(yCombined[i], a * y1[i] + b * y2[i], math::Tolerance<float>());
 }
 
 TEST_F(TestConvolutionCorrelation, circular_convolution_wraps)
@@ -128,7 +167,45 @@ TEST_F(TestConvolutionCorrelation, circular_convolution_wraps)
     EXPECT_NEAR(y[3], 5.0f, math::Tolerance<float>());
 }
 
-TEST_F(TestConvolutionCorrelation, autocorrelation_peaks_at_zero_lag)
+TEST_F(TestConvolutionCorrelation, autocorrelation_values_match_closed_form)
+{
+    Vec4 x;
+    x.push_back(1.0f);
+    x.push_back(1.0f);
+    x.push_back(1.0f);
+    x.push_back(1.0f);
+
+    Vec7 r;
+    analysis::AutoCorrelation<float, 4>(x, r);
+
+    EXPECT_NEAR(r[0], 1.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[1], 2.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[2], 3.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[3], 4.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[4], 3.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[5], 2.0f, math::Tolerance<float>());
+    EXPECT_NEAR(r[6], 1.0f, math::Tolerance<float>());
+}
+
+TEST_F(TestConvolutionCorrelation, autocorrelation_zero_lag_equals_signal_energy)
+{
+    Vec4 x;
+    x.push_back(1.0f);
+    x.push_back(2.0f);
+    x.push_back(-1.0f);
+    x.push_back(3.0f);
+
+    Vec7 r;
+    analysis::AutoCorrelation<float, 4>(x, r);
+
+    float energy{ 0.0f };
+    for (std::size_t i = 0; i < 4; ++i)
+        energy += x[i] * x[i];
+
+    EXPECT_NEAR(r[3], energy, math::Tolerance<float>());
+}
+
+TEST_F(TestConvolutionCorrelation, autocorrelation_is_symmetric)
 {
     Vec4 x;
     x.push_back(1.0f);
@@ -147,7 +224,7 @@ TEST_F(TestConvolutionCorrelation, autocorrelation_peaks_at_zero_lag)
     EXPECT_NEAR(r[2], r[4], math::Tolerance<float>());
 }
 
-TEST_F(TestConvolutionCorrelation, cross_correlation_finds_known_delay)
+TEST_F(TestConvolutionCorrelation, cross_correlation_values_match_hand_calc)
 {
     using Vec5 = infra::BoundedVector<float>::WithMaxSize<5>;
     using Vec9 = infra::BoundedVector<float>::WithMaxSize<9>;
@@ -171,6 +248,32 @@ TEST_F(TestConvolutionCorrelation, cross_correlation_finds_known_delay)
 
     std::size_t lag{ analysis::ArgMaxLag<float, 9>(r) };
     EXPECT_EQ(lag, 1u);
+
+    EXPECT_NEAR(r[1], 1.0f, math::Tolerance<float>());
+    for (std::size_t i = 0; i < 9; ++i)
+    {
+        if (i != 1)
+            EXPECT_NEAR(r[i], 0.0f, math::Tolerance<float>());
+    }
+}
+
+TEST_F(TestConvolutionCorrelation, linear_convolution_zero_signal_gives_zero_output)
+{
+    Vec3 x;
+    x.push_back(0.0f);
+    x.push_back(0.0f);
+    x.push_back(0.0f);
+
+    Vec3 h;
+    h.push_back(1.0f);
+    h.push_back(2.0f);
+    h.push_back(3.0f);
+
+    Vec5 y;
+    analysis::LinearConvolution<float, 3, 3>(x, h, y);
+
+    for (std::size_t i = 0; i < 5; ++i)
+        EXPECT_NEAR(y[i], 0.0f, math::Tolerance<float>());
 }
 
 TEST_F(TestConvolutionCorrelation, fast_convolution_matches_direct)
@@ -196,24 +299,4 @@ TEST_F(TestConvolutionCorrelation, fast_convolution_matches_direct)
 
     for (std::size_t i = 0; i < 5; ++i)
         EXPECT_NEAR(yFast[i], yDirect[i], 1e-3f);
-}
-
-TEST_F(TestConvolutionCorrelation, single_tap_scales_signal)
-{
-    Vec3 x;
-    x.push_back(1.0f);
-    x.push_back(2.0f);
-    x.push_back(3.0f);
-
-    Vec3 h;
-    h.push_back(2.5f);
-    h.push_back(0.0f);
-    h.push_back(0.0f);
-
-    Vec5 y;
-    analysis::LinearConvolution<float, 3, 3>(x, h, y);
-
-    EXPECT_NEAR(y[0], 2.5f, math::Tolerance<float>());
-    EXPECT_NEAR(y[1], 5.0f, math::Tolerance<float>());
-    EXPECT_NEAR(y[2], 7.5f, math::Tolerance<float>());
 }
