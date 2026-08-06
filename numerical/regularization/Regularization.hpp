98b01e7ead05d09d9745f1cc02b7e3569fa57796
gfs <gabriel.fra.santos@gmail.com>
2026-08-06 17:21:43 +0000

    fix build

--- numerical/regularization/Regularization.hpp
+++ numerical/regularization/Regularization.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "numerical/math/Matrix.hpp"
+#include "numerical/math/QNumber.hpp"
 
 namespace regularization
 {
     template<typename T, std::size_t Size>
     class Regularization
     {
-        static_assert(std::is_floating_point_v<T>, "Regularization supports floating-point types");
+        static_assert(std::is_floating_point_v<T> || math::is_qnumber_v<T>, "Regularization supports floating-point and QNumber types");
 
     public:
         using Vector = math::Vector<T, Size>;
