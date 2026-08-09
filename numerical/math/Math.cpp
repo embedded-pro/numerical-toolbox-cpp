c9caba9e93a1bbbc64d5f1e85dbf654bf56ca34d
gfs <gabriel.fra.santos@gmail.com>
2026-08-09 11:34:46 +0000

    fix sonar issues

--- numerical/math/Math.cpp
+++ numerical/math/Math.cpp
@@ -3,76 +3,76 @@
 #ifdef NUMERICAL_TOOLBOX_COVERAGE_BUILD
 
 #ifndef MATH_ABS_OVERRIDE
-namespace math { template float Abs<float>(float); }
+template float math::Abs<float>(float);
 #endif
 #ifndef MATH_SQRT_OVERRIDE
-namespace math { template float Sqrt<float>(float); }
+template float math::Sqrt<float>(float);
 #endif
 #ifndef MATH_SIN_OVERRIDE
-namespace math { template float Sin<float>(float); }
+template float math::Sin<float>(float);
 #endif
 #ifndef MATH_COS_OVERRIDE
-namespace math { template float Cos<float>(float); }
+template float math::Cos<float>(float);
 #endif
 #ifndef MATH_TAN_OVERRIDE
-namespace math { template float Tan<float>(float); }
+template float math::Tan<float>(float);
 #endif
 #ifndef MATH_ASIN_OVERRIDE
-namespace math { template float Asin<float>(float); }
+template float math::Asin<float>(float);
 #endif
 #ifndef MATH_ACOS_OVERRIDE
-namespace math { template float Acos<float>(float); }
+template float math::Acos<float>(float);
 #endif
 #ifndef MATH_ATAN_OVERRIDE
-namespace math { template float Atan<float>(float); }
+template float math::Atan<float>(float);
 #endif
 #ifndef MATH_ATAN2_OVERRIDE
-namespace math { template float Atan2<float>(float, float); }
+template float math::Atan2<float>(float, float);
 #endif
 #ifndef MATH_EXP_OVERRIDE
-namespace math { template float Exp<float>(float); }
+template float math::Exp<float>(float);
 #endif
 #ifndef MATH_LOG_OVERRIDE
-namespace math { template float Log<float>(float); }
+template float math::Log<float>(float);
 #endif
 #ifndef MATH_LOG10_OVERRIDE
-namespace math { template float Log10<float>(float); }
+template float math::Log10<float>(float);
 #endif
 #ifndef MATH_LOG2_OVERRIDE
-namespace math { template float Log2<float>(float); }
+template float math::Log2<float>(float);
 #endif
 #ifndef MATH_POW_OVERRIDE
-namespace math { template float Pow<float>(float, float); }
+template float math::Pow<float>(float, float);
 #endif
 #ifndef MATH_SINH_OVERRIDE
-namespace math { template float Sinh<float>(float); }
+template float math::Sinh<float>(float);
 #endif
 #ifndef MATH_COSH_OVERRIDE
-namespace math { template float Cosh<float>(float); }
+template float math::Cosh<float>(float);
 #endif
 #ifndef MATH_TANH_OVERRIDE
-namespace math { template float Tanh<float>(float); }
+template float math::Tanh<float>(float);
 #endif
 #ifndef MATH_HYPOT_OVERRIDE
-namespace math { template float Hypot<float>(float, float); }
+template float math::Hypot<float>(float, float);
 #endif
 #ifndef MATH_COPYSIGN_OVERRIDE
-namespace math { template float Copysign<float>(float, float); }
+template float math::Copysign<float>(float, float);
 #endif
 #ifndef MATH_FMOD_OVERRIDE
-namespace math { template float Fmod<float>(float, float); }
+template float math::Fmod<float>(float, float);
 #endif
 #ifndef MATH_CEIL_OVERRIDE
-namespace math { template float Ceil<float>(float); }
+template float math::Ceil<float>(float);
 #endif
 #ifndef MATH_FLOOR_OVERRIDE
-namespace math { template float Floor<float>(float); }
+template float math::Floor<float>(float);
 #endif
 #ifndef MATH_ROUND_OVERRIDE
-namespace math { template float Round<float>(float); }
+template float math::Round<float>(float);
 #endif
 #ifndef MATH_ERFC_OVERRIDE
-namespace math { template float Erfc<float>(float); }
+template float math::Erfc<float>(float);
 #endif
 
 #endif
